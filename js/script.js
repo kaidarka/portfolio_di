d918da96f81a8bd83466c5c976fbaeef5eacfe41
kaidarka <70837397+kaidarka@users.noreply.github.com>
2020-12-13 01:22:45 +0300

    Адаптив ДО моих работ

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("header__burger").onclick = function() {
+    document.getElementById("header__burger").classList.toggle('active');
+    document.getElementById("header__menu").classList.toggle('active');
+  }
